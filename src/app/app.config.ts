64a24047a1db91fe13663752eae2159feab26649
VONG Jeremy <gnov.jeremy@gmail.com>
2025-05-21 04:37:25 +0200

    Added base skeletal angular file

---
+++ src/app/app.config.ts
@@ -0,0 +1,27 @@
+/*!
+ * @license
+ * Copyright Google LLC All Rights Reserved.
+ *
+ * Use of this source code is governed by an MIT-style license that can be
+ * found in the LICENSE file at https://angular.dev/license
+ */
+
+// Angular 19
+
+import { provideHttpClient, withFetch } from "@angular/common/http";
+import { ApplicationConfig } from "@angular/core";
+import {
+  ModuleRegistry,
+  AllCommunityModule,
+  ClientSideRowModelModule, // or AllEnterpriseModule
+} from "ag-grid-community";
+
+// Register the module
+ModuleRegistry.registerModules([
+  AllCommunityModule,
+  ClientSideRowModelModule, // or AllEnterpriseModule
+]);
+
+export const appConfig: ApplicationConfig = {
+  providers: [provideHttpClient()],
+};
