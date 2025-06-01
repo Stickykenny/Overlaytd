b3dc1dac9631f93de8a50dc59a89ac5c373995bc
VONG Jeremy <gnov.jeremy@gmail.com>
2025-06-01 23:07:45 +0200

    Auto-login if JWT token not expired, also small clean-up

--- src/app/api.service.ts
+++ src/app/api.service.ts
@@ -2,19 +2,33 @@ import { HttpClient, HttpHeaders } from "@angular/common/http";
 import { Injectable } from "@angular/core";
 
 import { Astre } from "./models/Astre";
+import { BehaviorSubject, Observable } from "rxjs";
+import { jwtDecode, JwtPayload } from "jwt-decode";
 
 @Injectable({
   providedIn: "root",
 })
 export class ApiService {
   isLogged = false;
-  constructor(private http: HttpClient) {}
+  private loggedInSubject = new BehaviorSubject<boolean>(false);
+
+  // Observable for other components to subscribe to
+  isLoggedIn$: Observable<boolean> = this.loggedInSubject.asObservable();
+  constructor(private http: HttpClient) {
+    const token = localStorage.getItem("jwt");
+    if (token != null) {
+      var decoded = jwtDecode<JwtPayload>(token);
+      if (decoded.exp != null) {
+        if (Date.now() < decoded.exp * 1000) {
+          this.loggedInSubject.next(true);
+        }
+      }
+    }
+  }
 
   public login(username: string, password: string) {
-    let username1 = "visitor";
-    let password1 = "password";
     const headers = new HttpHeaders({
-      Authorization: "Basic " + btoa(username1 + ":" + password1),
+      Authorization: "Basic " + btoa(username + ":" + password),
       // btoa binary to ascii
     });
     headers.set("access-control-allow-origin", "http://localhost:4200/");
@@ -30,7 +44,7 @@ export class ApiService {
     response.subscribe({
       next: (data: string) => {
         localStorage.setItem("jwt", data);
-        this.isLogged = true;
+        this.loggedInSubject.next(true);
       },
       error: (err) => {
         console.error("Login failed:", err);
@@ -38,25 +52,15 @@ export class ApiService {
     });
   }
 
-  isLoggedIn() {
-    return this.isLogged;
-  }
-
   getAstres() {
-    let username = "visitor";
-    let password = "password";
-    const headers = new HttpHeaders({
-      //Authorization: "Basic " + btoa(username + ":" + password),
-    });
+    const headers = new HttpHeaders({});
     return this.http.get<Astre[]>("http://localhost:8080/api/astres/getall", {
       headers,
     });
   }
 
   postAstres(astres: Astre[]) {
-    const headers = new HttpHeaders({
-      //Authorization: "Bearer " + localStorage.getItem("jwt"),
-    });
+    const headers = new HttpHeaders({});
     headers.set("access-control-allow-origin", "http://localhost:4200/");
     return this.http.post<Astre[]>(
       "http://localhost:8080/api/astres/astres",
